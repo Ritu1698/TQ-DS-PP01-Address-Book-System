184e0e38b8a4cd0dee4af0d143abbabb5b8bf11c
Ritu1698 <biswasrituparna83@gmail.com>
2020-09-26 16:18:59 +0530

    [Rituparna]Added: Logic to Add Contact to AddBook

--- src/com/bridgelabz/AddBookSys/AddressBookMain.java
+++ src/com/bridgelabz/AddBookSys/AddressBookMain.java
@@ -1,12 +1,54 @@
 package com.bridgelabz.AddBookSys;
+import java.util.*;
 
 public class AddressBookMain {
 	
-	public static void main(String[] args) {
-		
-		
-		System.out.println("Welcome to Address Book Program in AddressBookMain class on Master Branch \n");
-		
-	}
+	ArrayList<Contact> persons;
+	Scanner sc= new Scanner(System.in);
+	 
 	
+	      public AddressBookMain() 
+	      {
+	            persons = new ArrayList<Contact>();
+	      }
+	      
+	      public void AddName()
+	      {
+	    	
+	    	  String firstname,lastname,address,city,state,zip,number,email;
+	    	  System.out.println("Enter your details:\n");
+	    	  System.out.println("Firstname\n");
+	    	  firstname=sc.nextLine();
+	    	  System.out.println("Lastname\n");
+	    	  lastname=sc.nextLine();
+	    	  System.out.println("Address\n");
+	    	  address=sc.nextLine();
+	    	  System.out.println("City\n");
+	    	  city=sc.nextLine();
+	    	  System.out.println("State\n");
+	    	  state=sc.nextLine();
+	    	  System.out.println("Zip\n");
+	    	  zip=sc.nextLine();
+	    	  System.out.println("Phone No.\n");
+	    	  number=sc.nextLine();
+	    	  System.out.println("Email\n");
+	    	  email=sc.nextLine();
+	    	  Contact c= new Contact(firstname, lastname, address, city, state, zip, number, email);
+	    	  persons.add(c);
+	    	  System.out.println("Contact Succesfully Added!!!\n\nContactList Contains\n");
+	    	  for (int i = 0; i < persons.size(); i++)
+	    	  { 
+	    		   System.out.print(i+1+". "+persons.get(i).firstname + " "); 
+	          } 
+	    	  
+	      }
+	      
+	      public static void main(String[] args) {
+	    	  System.out.println("Welcome to Address Book Program in AddressBookMain class on Master Branch \n");
+	    	  AddressBookMain a= new AddressBookMain();
+	    	  a.AddName();
+		}
+	      
+	 
+
 }
