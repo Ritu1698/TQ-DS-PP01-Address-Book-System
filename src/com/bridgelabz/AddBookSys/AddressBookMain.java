7cb3fcd002e8e70bb7456f8af6e88b23059b0d71
Ritu1698 <biswasrituparna83@gmail.com>
2020-09-26 17:19:58 +0530

    [Rituparna]Added: Edit Option Added

--- src/com/bridgelabz/AddBookSys/AddressBookMain.java
+++ src/com/bridgelabz/AddBookSys/AddressBookMain.java
@@ -3,52 +3,160 @@ import java.util.*;
 
 public class AddressBookMain {
 	
-	ArrayList<Contact> persons;
-	Scanner sc= new Scanner(System.in);
-	 
+	public static ArrayList<Contact> persons;
 	
-	      public AddressBookMain() 
-	      {
-	            persons = new ArrayList<Contact>();
-	      }
-	      
-	      public void AddName()
-	      {
-	    	
-	    	  String firstname,lastname,address,city,state,zip,number,email;
-	    	  System.out.println("Enter your details:\n");
-	    	  System.out.println("Firstname\n");
-	    	  firstname=sc.nextLine();
-	    	  System.out.println("Lastname\n");
-	    	  lastname=sc.nextLine();
-	    	  System.out.println("Address\n");
-	    	  address=sc.nextLine();
-	    	  System.out.println("City\n");
-	    	  city=sc.nextLine();
-	    	  System.out.println("State\n");
-	    	  state=sc.nextLine();
-	    	  System.out.println("Zip\n");
-	    	  zip=sc.nextLine();
-	    	  System.out.println("Phone No.\n");
-	    	  number=sc.nextLine();
-	    	  System.out.println("Email\n");
-	    	  email=sc.nextLine();
-	    	  Contact c= new Contact(firstname, lastname, address, city, state, zip, number, email);
-	    	  persons.add(c);
-	    	  System.out.println("Contact Succesfully Added!!!\n\nContactList Contains\n");
-	    	  for (int i = 0; i < persons.size(); i++)
-	    	  { 
-	    		   System.out.print(i+1+". "+persons.get(i).firstname + " "); 
-	          } 
-	    	  
-	      }
-	      
-	      public static void main(String[] args) {
-	    	  System.out.println("Welcome to Address Book Program in AddressBookMain class on Master Branch \n");
-	    	  AddressBookMain a= new AddressBookMain();
-	    	  a.AddName();
-		}
-	      
+	
+	 Scanner sc = new Scanner(System.in);
+	 Scanner sc1 = new Scanner(System.in);
+	 Scanner sc2 = new Scanner(System.in);
 	 
-
+	 
+	 
+	 public AddressBookMain(){
+		 
+	       persons = new ArrayList<Contact>();    
+	 }
+	 
+	 
+	 public static Contact SearchByFirstName(String firstname){
+   	  
+		 Contact x = new Contact();
+		 
+   	     for(int i=0;i<persons.size();i++){
+   	    	 
+   	    	 Contact y = (Contact)persons.get(i);
+   	    	 if(firstname.equals(y.firstname)){
+   	    		 
+   			   x = y;
+   		     }
+   		  
+   	     }
+   	     
+		 return x;
+   	  
+     }
+	 
+	 public void AddName()
+     {
+		 String firstname,lastname,address,city,state,zip,number,email;
+   	     System.out.println("Enter your details:\n");
+   	     System.out.println("Firstname\n");
+   	     firstname=sc.nextLine();
+   	     System.out.println("Lastname\n");
+   	     lastname=sc.nextLine();
+   	     System.out.println("Address\n");
+   	     address=sc.nextLine();
+   	     System.out.println("City\n");
+   	     city=sc.nextLine();
+   	     System.out.println("State\n");
+   	     state=sc.nextLine();
+   	     System.out.println("Zip\n");
+   	     zip=sc.nextLine();
+   	     System.out.println("Phone No.\n");
+   	     number=sc.nextLine();
+   	     System.out.println("Email\n");
+   	     email=sc.nextLine();
+   	     Contact c= new Contact(firstname, lastname, address, city, state, zip, number, email);
+   	     persons.add(c);
+   	     System.out.println("Contact Succesfully Added!!!\n\nContactList Contains\n");
+   	     
+   	     for (int i = 0; i < persons.size(); i++){ 
+   	    	System.out.println(i+1+". Firstname "+persons.get(i).firstname+" Lastname "+persons.get(i).lastname+
+   	    			" Address "+persons.get(i).address+" City "+persons.get(i).city+" State "+persons.get(i).state+
+   	    			" Zip "+persons.get(i).zip+" Phone number "+persons.get(i).number+" Email "+persons.get(i).email+"\n"); 
+         } 
+   	 
+     }
+	 
+	 public void EditDetailsByFirstName(String firstname) {
+		 
+		 System.out.println("Enter Choice of what you want to Edit\n\n\n1.FirstName\n2.Lastname\n3.Address\n4.City\n5.State\n6.Zip\n7.Phone\n8.Email\n");
+		 Contact a = SearchByFirstName(firstname);
+		 int choice = sc1.nextInt();
+		 
+		 switch(choice) {
+		 
+		 case 1:
+			 System.out.println("Enter new firstname: \n");
+   		     a.firstname = sc2.nextLine();
+   		     break;
+   		 
+		 case 2:
+			 System.out.println("Enter new lastname: \n");
+   		     a.lastname = sc2.nextLine();
+   		     break;
+   		     
+		 case 3:
+			 System.out.println("Enter new address: \n");
+   		     a.address = sc2.nextLine();
+   		     break;
+   		     
+		 case 4:
+			 System.out.println("Enter new city: \n");
+   		     a.city = sc2.nextLine();
+   		     break;
+   		     
+		 case 5:
+			 System.out.println("Enter new state: \n");
+   		     a.state = sc2.nextLine();
+   		     break;
+   		     
+		 case 6:
+			 System.out.println("Enter new zip: \n");
+   		     a.zip = sc2.nextLine();
+   		     break;
+   		     
+		 case 7:
+			 System.out.println("Enter new ph. number: \n");
+   		     a.number = sc2.nextLine();
+   		     break;
+   		     
+		 case 8:
+			 System.out.println("Enter new email id: \n");
+   		     a.email = sc2.nextLine();
+   		     break;
+		 default:
+			 break;	 
+		 }
+		 
+         System.out.println("Contact Succesfully Edited!!!\n\nContactList Contains\n");
+   	     for (int i = 0; i < persons.size(); i++){
+   	    	System.out.println(i+1+". Firstname "+persons.get(i).firstname+" Lastname "+persons.get(i).lastname+
+   	    			" Address "+persons.get(i).address+" City "+persons.get(i).city+" State "+persons.get(i).state+
+   	    			" Zip "+persons.get(i).zip+" Phone number "+persons.get(i).number+" Email "+persons.get(i).email+"\n"); 
+         } 
+		 
+		 
+		 
+	 }
+	 
+	 public static void main(String[] args) {
+		 
+		 Scanner sc3 = new Scanner(System.in);
+		 System.out.println("Welcome to Address Book Program in AddressBookMain class on Master Branch \n");
+		 AddressBookMain a= new AddressBookMain();
+		 
+		 boolean flag=true;
+		 while(flag) {
+			 System.out.println("\n1.Add\n2.Edit\nEnter your choice : ");
+			 int choice = sc3.nextInt();
+			 switch(choice) {
+			 case 1:
+				  a.AddName();
+				  break;
+				  
+			  case 2:
+				  System.out.println("Enter name to edit");
+				  Scanner sc4 = new Scanner(System.in);
+				  String a1= sc4.nextLine();
+				  a.EditDetailsByFirstName(a1);
+				  break;  
+				  
+			  default:
+				  System.out.println("Select either 1 or 2, terminated!!!!");
+				  sc3.close();
+				  flag=false;
+			 } 
+		  }	 
+	   }
 }
